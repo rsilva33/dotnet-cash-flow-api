4d9170f41a0d2be184e89e614cc56357bb88bc9d
rsilva33 <ronaldo.silva.jr0105@gmail.com>
2024-04-15 10:50:46 -0300

    first commit

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,37 @@
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+
+        //to do validations
+        return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleEmpty = string.IsNullOrWhiteSpace(request.Title);
+
+        if(titleEmpty)
+            throw new ArgumentException("The title is required.");
+
+        if (request.Amount <= 0)
+            throw new ArgumentException("The Amount must be greater than zero.");
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+
+        if (result > 0)
+            throw new ArgumentException("Expenses cannot be fot the future.");
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+
+        if (paymentTypeIsValid is false)
+            throw new ArgumentException("Payment type is not valid.");
+    }
+}
---
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,12 @@
+﻿namespace CashFlow.Communication.Responses;
+public class ResponseErrorJson
+{
+    //Novas versoes do csharp podemos utilizar o required para substituir o construtor
+    //public required string ErrorMessage { get; set; } = string.Empty;
+    public string ErrorMessage { get; set; } = string.Empty;
+
+    public ResponseErrorJson(string errorMessage)
+    {
+        ErrorMessage = errorMessage;
+    }
+}
