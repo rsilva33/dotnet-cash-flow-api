d579146d5c822eba8097e50bd74082e8df92d505
rsilva33 <ronaldo.silva.jr0105@gmail.com>
2024-06-05 16:19:29 -0300

    Add title, header and value

--- src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Pdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,10 +1,14 @@
-﻿using MigraDoc.Rendering;
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Pdf.Colors;
+using MigraDoc.DocumentObjectModel.Tables;
+using MigraDoc.Rendering;
 
 namespace CashFlow.Application.UseCases.Expenses.Reports.Pdf;
 
 public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCase
 {
     private const string CURRENCY_SYMBOL = "€";
+    private const string FINAL_PART_OF_THE_LOGO_DIRECTORY = @"UseCases\Expenses\Reports\Pdf\Assets";
+    private const string LOGO_NAME = "ProfilePhoto.png";
 
     private readonly IExpensesReadOnlyRepository _repository;
 
@@ -19,25 +23,97 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
     {
         var expenses = await _repository.FilterByMonth(month);
 
-        if(expenses.Count is 0) 
+        if (expenses.Count is 0)
             return [];
 
         var document = CreateDocument(month);
-
         var page = CreatePage(document);
+        CreateHeaderWithProfilePhotoAndName(page);
 
+        var totalExpenses = expenses.Sum(expenses => expenses.Amount);
+        CreateTotalSpentSection(page, month, totalExpenses);
+
+        foreach (var expense in expenses)
+        {
+            var table = CreateExpenseTable(page);
+
+            var row = table.AddRow();
+            row.Height = 25;
+
+            row.Cells[0].AddParagraph(expense.Title);
+            row.Cells[0].Format.Font = new Font {  Name = FontHelper.RELAWAY_BLACK, Size = 14, Color = ColorsHelper.BLACK };
+            row.Cells[0].Shading.Color = ColorsHelper.RED_LIGHT;
+            row.Cells[0].VerticalAlignment = VerticalAlignment.Center;
+            row.Cells[0].MergeRight = 2;
+            row.Cells[0].Format.LeftIndent = 20;
+
+            row.Cells[3].AddParagraph(ResourceReportGenerationMessages.AMOUNT);
+            row.Cells[3].Format.Font = new Font { Name = FontHelper.RELAWAY_BLACK, Size = 14, Color = ColorsHelper.WHITE };
+            row.Cells[3].Shading.Color = ColorsHelper.RED_DARK;
+            row.Cells[3].VerticalAlignment = VerticalAlignment.Center;
+
+            row = table.AddRow();
+            row.Height = 25;
+
+            row.Cells[0].AddParagraph(expense.Date.ToString("D"));
+            row.Cells[0].Format.Font = new Font { Name = FontHelper.WORKSANS_REGULAR, Size = 12, Color = ColorsHelper.BLACK };
+            row.Cells[0].Shading.Color = ColorsHelper.GREEN_DARK;
+            row.Cells[0].VerticalAlignment = VerticalAlignment.Center;
+            row.Cells[0].Format.LeftIndent = 20;
+
+            row.Cells[1].AddParagraph(expense.Date.ToString("t"));
+            row.Cells[1].Format.Font = new Font { Name = FontHelper.WORKSANS_REGULAR, Size = 12, Color = ColorsHelper.BLACK };
+            row.Cells[1].Shading.Color = ColorsHelper.GREEN_DARK;
+            row.Cells[1].VerticalAlignment = VerticalAlignment.Center;
+
+            row.Cells[3].AddParagraph($"-{expense.Amount} {CURRENCY_SYMBOL}");
+            row.Cells[3].Format.Font = new Font { Name = FontHelper.WORKSANS_REGULAR, Size = 14, Color = ColorsHelper.BLACK };
+            row.Cells[3].Shading.Color = ColorsHelper.WHITE;
+            row.Cells[3].VerticalAlignment = VerticalAlignment.Center;
+
+            row = table.AddRow();
+            row.Height = 30;
+
+            row.Borders.Visible = false;
+        }
+
+        return RenderDocument(document);
+    }
+
+    private static void CreateHeaderWithProfilePhotoAndName(Section page)
+    {
+        var table = page.AddTable();
+       
+        table.AddColumn();
+        table.AddColumn("300");
+
+        var row = table.AddRow();
+
+        //recupera o assembly do projeto executado
+        var assembly = Assembly.GetExecutingAssembly();
+        var directoryName = Path.GetDirectoryName(assembly.Location);
+        var pathFile = Path.Combine(directoryName!, FINAL_PART_OF_THE_LOGO_DIRECTORY, LOGO_NAME);
+
+        row.Cells[0].AddImage(pathFile);
+
+        row.Cells[1].AddParagraph("Hey, Rona Silva");
+        row.Cells[1].Format.Font = new Font { Name = FontHelper.RELAWAY_BLACK, Size = 16 };
+        row.Cells[1].VerticalAlignment = VerticalAlignment.Center;
+    }
+
+    private static void CreateTotalSpentSection(Section page, DateOnly month, decimal totalExpenses)
+    {
         var paragraph = page.AddParagraph();
+      
+        paragraph.Format.SpaceBefore = "40";
+        paragraph.Format.SpaceAfter = "40";
 
         var title = string.Format(ResourceReportGenerationMessages.TOTAL_SPENT_IN, month.ToString("Y"));
 
         paragraph.AddFormattedText(title, new Font { Name = FontHelper.RELAWAY_REGULAR, Size = 15 });
         paragraph.AddLineBreak();
 
-        var totalExpenses = expenses.Sum(expenses => expenses.Amount);
-
-        paragraph.AddFormattedText($"{totalExpenses} {CURRENCY_SYMBOL}", new Font { Name = FontHelper.WORKSANS_BLACK, Size = 50 });
-
-        return RenderDocument(document);
+        paragraph.AddFormattedText($"{totalExpenses:N2} {CURRENCY_SYMBOL}", new Font { Name = FontHelper.WORKSANS_BLACK, Size = 50 });
     }
 
     private Document CreateDocument(DateOnly month)
@@ -67,6 +143,21 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
         section.PageSetup.BottomMargin = 80;
 
         return section;
+    }
+
+    private Table CreateExpenseTable(Section page)
+    {
+        var table = page.AddTable();
+
+        //Sempre comecar com as colunas
+        table.AddColumn("195").Format.Alignment = ParagraphAlignment.Left;
+        table.AddColumn("80").Format.Alignment = ParagraphAlignment.Center;
+        table.AddColumn("120").Format.Alignment = ParagraphAlignment.Center;
+        table.AddColumn("120").Format.Alignment = ParagraphAlignment.Right;
+
+        table.AddRow();
+
+        return table;
     }
 
     private byte[] RenderDocument(Document document)
