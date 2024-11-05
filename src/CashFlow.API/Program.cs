b13d1312fa3355c589804addfbffeec9563e32ae
rsilva33 <ronaldo.silva.jr0105@gmail.com>
2024-11-05 15:35:17 -0300

    Starting integration test

--- src/CashFlow.API/Program.cs
+++ src/CashFlow.API/Program.cs
@@ -83,3 +83,5 @@ async Task MigrateDataBase()
 
     await DataBaseMigration.MigrateDataBase(scope.ServiceProvider);
 }
+
+public partial class Program { }
---
+++ src/WebApi.Test/Register/Users/RegisterUserTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace WebApi.Test.Register.Users;
+
+public class RegisterUserTest : IClassFixture<WebApplicationFactory<Program>>
+{
+    [Fact]
+    public async Task Success()
+    {
+        
+    }
+}
