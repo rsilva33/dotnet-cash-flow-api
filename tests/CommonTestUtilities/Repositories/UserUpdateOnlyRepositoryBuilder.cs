a794bc73897ddbecb91574a24dedec27e94dfc1b
rsilva33 <ronaldo.silva.jr0105@gmail.com>
2025-03-03 16:02:31 -0300

    Unit tests to update user profiles

---
+++ tests/CommonTestUtilities/Repositories/UserUpdateOnlyRepositoryBuilder.cs
@@ -0,0 +1,15 @@
+using CashFlow.Domain.Repositories;
+
+namespace CommonTestUtilities.Repositories;
+
+public class UserUpdateOnlyRepositoryBuilder
+{
+    public static IUserUpdateOnlyRepository Build(User user)
+    {
+        var mock = new Mock<IUserUpdateOnlyRepository>();
+
+        mock.Setup(repository => repository.GetById(user.Id)).ReturnsAsync(user);
+
+        return mock.Object;
+    }
+}
