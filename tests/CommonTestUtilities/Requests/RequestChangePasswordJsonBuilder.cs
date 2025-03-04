0d8bd51447ef34ec7755d31079f13d96041912ae
rsilva33 <ronaldo.silva.jr0105@gmail.com>
2025-03-04 14:35:48 -0300

    Unit tests for changing password

---
+++ tests/CommonTestUtilities/Requests/RequestChangePasswordJsonBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace CommonTestUtilities.Requests;
+
+public class RequestChangePasswordJsonBuilder
+{
+    public static RequestChangePasswordJson Build()
+    {
+        return new Faker<RequestChangePasswordJson>()
+            .RuleFor(user => user.Password, faker => faker.Internet.Password())
+            .RuleFor(user => user.NewPassword, faker => faker.Internet.Password(prefix: "!Aa1"));
+    }
+}
