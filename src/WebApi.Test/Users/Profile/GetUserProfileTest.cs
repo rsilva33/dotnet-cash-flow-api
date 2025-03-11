267a960f8dda8724f3b0e0320cf4869d6da2eb8c
rsilva33 <ronaldo.silva.jr0105@gmail.com>
2025-03-11 08:12:27 -0300

    tests: adding tests for delete user profile

--- src/WebApi.Test/Users/Profile/GetUserProfileTest.cs
+++ src/WebApi.Test/Users/Profile/GetUserProfileTest.cs
@@ -1,5 +1,8 @@
-using System.Net;
-using System.Text.Json;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace WebApi.Test.Users.Profile;
 
---
+++ tests/UseCases.Tests/Users/Delete/DeleteUserAccountUseCaseTest.cs
@@ -0,0 +1,26 @@
+﻿using CashFlow.Application.UseCases.Users.Delete;
+
+namespace UseCases.Tests.Users.Delete;
+
+public class DeleteUserAccountUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var user = UserBuilder.Build();
+        var useCase = CreateUseCase(user);
+
+        var act = async () => await useCase.Execute();
+
+        await act.Should().NotThrowAsync();
+    }
+
+    private DeleteUserAccountUseCase CreateUseCase(User user)
+    {
+        var repository = UserWriteOnlyRepositoryBuilder.Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+        var unitOfWork = UnitOfWorkBuilder.Build();
+
+        return new DeleteUserAccountUseCase(unitOfWork, repository, loggedUser);
+    }
+}
